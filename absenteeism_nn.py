c2bc8aa2e35c493869de4c519fd810e516586e13
Aditya Sinha <u6010425@anu.edu.au>
2018-04-25 19:47:46 +1000

    final commit for today

--- absenteeism_nn.py
+++ absenteeism_nn.py
@@ -5,13 +5,17 @@ import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-# Load data
+"""
+Section One: Preprocessing data
+"""
+
+# load data
 data = pd.read_csv('Dataset/Absenteeism_at_work.csv', sep=';')
 
-# Drop id column (it's not useful)
+# drop id column (it's not useful)
 data.drop(data.columns[0], axis=1, inplace=True)
 
-# One hot encode categorical variable
+# one hot encode categorical variable
 one_hot = pd.get_dummies(data['Reason for absence'])
 data = data.drop('Reason for absence', axis=1)
 data = one_hot.join(data)
@@ -35,7 +39,7 @@ for a in data.iloc[:, 46:47]:
 print(data.iloc[0:10, 0:10])
 """
 
-# Shuffle data
+# shuffle data
 data = data.sample(frac=1).reset_index(drop=True)
 
 # randomly split data into training set (80%) and testing set (20%)
@@ -52,3 +56,12 @@ train_target = train_data.iloc[:, n_features]
 # split test data into input and target
 test_input = test_data.iloc[:, :n_features]
 test_target = test_data.iloc[:, n_features]
+
+# create Tensors and wrap in Variables
+X = Variable(torch.Tensor(train_input.as_matrix()).float())
+Y = Variable(torch.Tensor(train_target.as_matrix()).long())
+
+"""
+Section Two: Building the neural network
+"""
+
