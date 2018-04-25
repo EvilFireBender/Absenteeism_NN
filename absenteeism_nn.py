4bea00cab52a83ec7513d45a2715bc2361b72336
Aditya Sinha <u6010425@anu.edu.au>
2018-04-25 17:15:56 +1000

    more work

--- absenteeism_nn.py
+++ absenteeism_nn.py
@@ -7,29 +7,33 @@ from torch.autograd import Variable
 
 # Load data
 data = pd.read_csv('Dataset/Absenteeism_at_work.csv', sep=';')
-print(type(data))
 
 # Drop id column (it's not useful)
 data.drop(data.columns[0], axis=1, inplace=True)
-print(data.iloc[:, 0:4])
+
 
 # One hot encode categorical variable
 one_hot = pd.get_dummies(data['Reason for absence'])
 data = data.drop('Reason for absence', axis=1)
-data = data.join(one_hot)
+data = one_hot.join(data)
 
-# Join the one-hot-encoded columns back in the location of the original column or else
-# the next bit will normalise the target column
 
 # normalise input data
 for column in data:
     # the last column is target
-    if column != data.shape[1] - 1:
+    if column != 'Absenteeism time in hours':
         data[column] = data.loc[:, [column]].apply(lambda x: (x - x.mean()) / x.std())
     # or divide by x.max() - x.min()
 
-print(data.iloc[:, 15:])
+# Divide the output data into three categories
+for a in data[:, -1:]:
+    print(data[a])
+
+#print(data.iloc[0:10, 0:10])
+
 # Shuffle data
+data = data.sample(frac=1).reset_index(drop=True)
+
 
 
 # Split into training set and testing set
