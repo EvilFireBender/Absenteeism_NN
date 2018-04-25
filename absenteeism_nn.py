7ed5e531ce39fa8a2043cf4402908e6a52c3f776
Aditya Sinha <u6010425@anu.edu.au>
2018-04-25 18:40:33 +1000

    Finally done with preprocessing

--- absenteeism_nn.py
+++ absenteeism_nn.py
@@ -11,12 +11,10 @@ data = pd.read_csv('Dataset/Absenteeism_at_work.csv', sep=';')
 # Drop id column (it's not useful)
 data.drop(data.columns[0], axis=1, inplace=True)
 
-
 # One hot encode categorical variable
 one_hot = pd.get_dummies(data['Reason for absence'])
 data = data.drop('Reason for absence', axis=1)
 data = one_hot.join(data)
-
 
 # normalise input data
 for column in data:
@@ -26,14 +24,31 @@ for column in data:
     # or divide by x.max() - x.min()
 
 # Divide the output data into three categories
-for a in data[:, -1:]:
-    print(data[a])
+data.at[data['Absenteeism time in hours'] <= 20, ['Absenteeism time in hours']] = 0
+data.at[data['Absenteeism time in hours'] > 80, ['Absenteeism time in hours']] = 2
+data.at[data['Absenteeism time in hours'] > 2, ['Absenteeism time in hours']] = 1
 
-#print(data.iloc[0:10, 0:10])
+
+"""
+for a in data.iloc[:, 46:47]:
+    print(data[a])
+print(data.iloc[0:10, 0:10])
+"""
 
 # Shuffle data
 data = data.sample(frac=1).reset_index(drop=True)
 
+# randomly split data into training set (80%) and testing set (20%)
+msk = np.random.rand(len(data)) < 0.8
+train_data = data[msk]
+test_data = data[~msk]
 
 
-# Split into training set and testing set
+# split training data into input and target
+n_features = train_data.shape[1] - 1
+train_input = train_data.iloc[:, :n_features]
+train_target = train_data.iloc[:, n_features]
+
+# split test data into input and target
+test_input = test_data.iloc[:, :n_features]
+test_target = test_data.iloc[:, n_features]
