d5a0d3bbe78845a6988e0960520a8b2bf0d4fc9b
Aditya  Sinha <u6010425@anu.edu.au>
2018-05-29 15:49:59 +1000

    "adding biases"

--- absenteeism_nn.py
+++ absenteeism_nn.py
@@ -4,9 +4,10 @@ import pandas as pd
 import torch
 import torch.nn.functional as F
 from torch.autograd import Variable
+import matplotlib.pyplot as plt
 
 """
-Section One: Preprocessing data
+Section 1: Preprocessing data
 """
 
 # load data
@@ -20,33 +21,31 @@ one_hot = pd.get_dummies(data['Reason for absence'])
 data = data.drop('Reason for absence', axis=1)
 data = one_hot.join(data)
 
+# one_hot2 = pd.get_dummies(data['Month of absence'])
+# data = data.drop('Month of absence', axis=1)
+# data = one_hot2.join(data)
+
 # normalise input data
 for column in data:
     # the last column is target
     if column != 'Absenteeism time in hours':
         data[column] = data.loc[:, [column]].apply(lambda x: (x - x.mean()) / x.std())
-    # or divide by x.max() - x.min()
+        # or divide by x.max() - x.min()
 
 # Divide the output data into three categories
 data.at[data['Absenteeism time in hours'] <= 10, ['Absenteeism time in hours']] = 0
 data.at[data['Absenteeism time in hours'] > 40, ['Absenteeism time in hours']] = 2
 data.at[data['Absenteeism time in hours'] > 2, ['Absenteeism time in hours']] = 1
 
-
-"""
-for a in data.iloc[:, 46:47]:
-    print(data[a])
-print(data.iloc[0:10, 0:10])
-"""
+data.to_csv("Dataset/pre_processed_dataset.csv")
 
 # shuffle data
 data = data.sample(frac=1).reset_index(drop=True)
-# pd.set_option('display.max_rows', None)
+
 # randomly split data into training set (80%) and testing set (20%)
 msk = np.random.rand(len(data)) < 0.8
 train_data = data[msk]
 test_data = data[~msk]
-#print(train_data.iloc[ :, 46:47])
 
 # split training data into input and target
 n_features = train_data.shape[1] - 1
@@ -62,11 +61,10 @@ X = Variable(torch.Tensor(train_input.as_matrix()).float())
 Y = Variable(torch.Tensor(train_target.as_matrix()).long())
 
 """
-Section Two: Building the neural network
+Section 2: Building the neural network
 """
-# define the number of neurons for input layer, hidden layer and output layer
-# define learning rate and number of epochs on training
-input_neurons = n_features # n_features = 46
+
+input_neurons = n_features  # n_features = 46
 # the number of hidden neurons usually lies between the number of input and output units. So we start with their mean
 hidden_neurons = 24
 output_neurons = 3
@@ -76,7 +74,6 @@ num_epoch = 5000
 
 # define a customised neural network structure
 class TwoLayerNet(torch.nn.Module):
-
     def __init__(self, n_input, n_hidden, n_output):
         super(TwoLayerNet, self).__init__()
         # define linear hidden layer output
@@ -85,11 +82,6 @@ class TwoLayerNet(torch.nn.Module):
         self.out = torch.nn.Linear(n_hidden, n_output)
 
     def forward(self, x):
-        """
-            In the forward function we define the process of performing
-            forward pass, that is to accept a Variable of input
-            data, x, and return a Variable of output data, y_pred.
-        """
         # get hidden layer input
         h_input = self.hidden(x)
         # define activation function for hidden layer
@@ -98,6 +90,7 @@ class TwoLayerNet(torch.nn.Module):
         y_pred = self.out(h_output)
 
         return y_pred
+
 
 # define a neural network using the customised structure
 net = TwoLayerNet(input_neurons, hidden_neurons, output_neurons)
@@ -108,9 +101,10 @@ loss_func = torch.nn.CrossEntropyLoss()
 # define optimiser
 optimiser = torch.optim.SGD(net.parameters(), lr=learning_rate)
 
-
 # store all losses for visualisation
 all_losses = []
+
+hidden_layer_biases = torch.zeros([24, 1])
 
 # train a neural network
 for epoch in range(num_epoch):
@@ -131,29 +125,52 @@ for epoch in range(num_epoch):
         correct = predicted.data.numpy() == Y.data.numpy()
 
         print('Epoch [%d/%d] Loss: %.4f  Accuracy: %.2f %%'
-              % (epoch + 1, num_epoch, loss.data[0], 100 * sum(correct)/total))
+              % (epoch + 1, num_epoch, loss.data[0], 100 * sum(correct) / total))
 
-    # Clear the gradients before running the backward pass.
     net.zero_grad()
 
-    # Perform backward pass
     loss.backward()
 
-    # Calling the step function on an Optimiser makes an update to its
-    # parameters
     optimiser.step()
 
+    # for child in net.children():
+    #     print(child)
+    #     for name, param in child.named_parameters():
+    #         if name == 'bias':
+    #             print(param)
+    #     break
 
-# Optional: plotting historical loss from ``all_losses`` during network learning
-# Please comment from next line to ``plt.show()`` if you don't want to plot loss
+    for child in net.children():
+        for name, param in child.named_parameters():
+            if name == 'bias':
+                if epoch == 0:
+                    hidden_layer_biases = param
+                else:
+                    hidden_layer_biases = hidden_layer_biases + param
+        break
 
-import matplotlib.pyplot as plt
 
+print(hidden_layer_biases)
+hidden_layer_biases = hidden_layer_biases.abs()
+
+
+# Plotting the error-epoch graph
 plt.figure()
 plt.plot(all_losses)
 plt.show()
 
-
+# for child in net.children():
+#     print(child)
+#     for name, param in child.named_parameters():
+#         print(name)
+#         print(param)
+#         if name == 'bias':
+#             hidden_layer_biases = param
+#     break
+#
+# print(hidden_layer_biases)
+# hidden_layer_biases = hidden_layer_biases + param
+# print(hidden_layer_biases)
 
 confusion = torch.zeros(output_neurons, output_neurons)
 
@@ -171,25 +188,14 @@ print('Confusion matrix for training:')
 print(confusion)
 
 """
-Step 3: Test the neural network
-
-Pass testing data to the built neural network and get its performance
+Section 3: Testing the neural network
 """
 
-# create Tensors to hold inputs and outputs, and wrap them in Variables,
-# as Torch only trains neural network on Variables
 X_test = Variable(torch.Tensor(test_input.as_matrix()).float())
 Y_test = Variable(torch.Tensor(test_target.as_matrix()).long())
 
-# test the neural network using testing data
-# It is actually performing a forward pass computation of predicted y
-# by passing x to the model.
-# Here, Y_pred_test contains three columns, where the index of the
-# max column indicates the class of the instance
 Y_pred_test = net(X_test)
 
-# get prediction
-# convert three-column predicted Y values to one column for comparison
 _, predicted_test = torch.max(Y_pred_test, 1)
 
 # calculate accuracy
@@ -199,11 +205,7 @@ correct_test = sum(predicted_test.data.numpy() == Y_test.data.numpy())
 print('Testing Accuracy: %.2f %%' % (100 * correct_test / total_test))
 
 """
-Evaluating the Results
-
-To see how well the network performs on different categories, we will
-create a confusion matrix, indicating for every iris flower (rows)
-which class the network guesses (columns). 
+Section 4: Evaluating the Results
 
 """
 
